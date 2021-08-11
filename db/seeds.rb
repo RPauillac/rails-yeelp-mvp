f3a4740db74e5351d6b8bec12c717ca9d80e3935
rahim pauillac <pauillacrahim@gmail.com>
2021-08-11 15:12:32 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'emptying database'
+Restaurant.destroy_all
+
+puts 'creating restaurants...'
+restaurant = Restaurant.create! name: 'McDO', address: '10 rue paris', phone_number: '0609050403', category: 'chinese'
+restaurant2 = Restaurant.create! name: 'BK', address: '11 rue paris', phone_number: '0609050403', category: 'japanese'
+
+puts 'creating reviews'
+review = Review.create! content: 'Good Food', rating: 4, restaurant: restaurant
+review = Review.create! content: 'Good Food', rating: 2, restaurant: restaurant
+review = Review.create! content: 'Good Food', rating: 4, restaurant: restaurant
+review = Review.create! content: 'Good Food', rating: 1, restaurant: restaurant2
+review = Review.create! content: 'Good Food', rating: 3, restaurant: restaurant2
+review = Review.create! content: 'Good Food', rating: 4, restaurant: restaurant2
