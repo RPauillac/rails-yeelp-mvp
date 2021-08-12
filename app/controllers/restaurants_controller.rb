6af3585ed4daa7c3b526d679ef8cab9fa6ae64af
rahim pauillac <pauillacrahim@gmail.com>
2021-08-12 11:03:30 +0200

    yelp affiche

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,19 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    @restaurant = Restaurant.new(params.require(:restaurant).permit(:name, :address, :phone_number, :category))
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
 end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,16 @@
+class ReviewsController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(params.require(:review).permit(:content))
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+end
